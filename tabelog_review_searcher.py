6437af2b2ed5781898a000ac28eb89e27a23695d
yu <yuya4.matsumura@gmail.com>
2016-11-10 23:17:29 +0900

    Delete needless instance variables

--- tabelog_review_searcher.py
+++ tabelog_review_searcher.py
@@ -11,9 +11,6 @@ class TabelogReviewSearcher:
 
     def __init__(self):
         self.url = 'https://tabelog.com/kyoto/0/0/rvw/COND-0-0-2-0/D-dt/'
-        # self.parameters = '/?rvw_part=all&sw=%E9%A3%B2%E3%82%80'
-        self.parameters = '/?rvw_part=all&sw=ちょっと飲む'
-
 
     def search(self, query):
         '''
