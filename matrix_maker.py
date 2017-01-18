d54753d6a8d512373a7690b9ef6521deded66da9
yu <yuya4.matsumura@gmail.com>
2017-01-18 23:23:31 +0900

    Add matrix_maker.py

---
+++ matrix_maker.py
@@ -0,0 +1,12 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8
+
+class matrixMaker:
+    '''
+    Making Action and Geographic feature matrix
+    '''
+
+def __init__(self):
+    '''
+    get actions list and geographic features list to make a matrix
+    '''
