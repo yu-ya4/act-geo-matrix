ed76c411797f2aace54762cba1ffbd4e445e6fce
yu <yuya4.matsumura@gmail.com>
2017-01-19 23:25:14 +0900

    Add prototype method

--- matrix_maker.py
+++ matrix_maker.py
@@ -1,12 +1,41 @@
 #!/usr/local/bin/python3
 # -*- coding: utf-8
 
-class matrixMaker:
+from act_geo_matrix import ActGeoMatrix
+
+class MatrixMaker:
     '''
-    Making Action and Geographic feature matrix
+    Making Action and Geographic feature matrix(ActGeoMatrix)
+    MatrixMaker has several ways of making matrix.
     '''
 
-def __init__(self):
-    '''
-    get actions list and geographic features list to make a matrix
-    '''
+    def __init__(self, actions_filename):
+        '''
+        get actions list and geographic features list to make a matrix
+        '''
+
+        self.actions = self.read_actions(actions_filename)
+
+    def read_actions(self, actions_filename):
+        '''
+        read actions list from text file
+
+        Args:
+            actions_filename: str
+        '''
+        f_a = open(actions_filename, 'r')
+        actions = [line.replace('\n', '') for line in f_a]
+        f_a.close()
+        return actions
+
+    def make(self):
+        '''
+        Args:
+
+        Return:
+            ActGeoMatrix
+        '''
+
+        matrix = ActGeoMatrix()
+
+        return matrix
