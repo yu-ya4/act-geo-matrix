c020c7ed9759834c0b90763a5e8912119fa63354
yu <yuya4.matsumura@gmail.com>
2017-01-23 17:39:54 +0900

    Add read_geos()

--- matrix_maker.py
+++ matrix_maker.py
@@ -9,12 +9,13 @@ class MatrixMaker:
     MatrixMaker has several ways of making matrix.
     '''
 
-    def __init__(self, actions_filename):
+    def __init__(self, actions_filename, geos_filename):
         '''
         get actions list and geographic features list to make a matrix
         '''
 
         self.actions = self.read_actions(actions_filename)
+        self.geos = self.read_geos(geos_filename)
 
     def read_actions(self, actions_filename):
         '''
@@ -27,6 +28,18 @@ class MatrixMaker:
         actions = [line.replace('\n', '') for line in f_a]
         f_a.close()
         return actions
+
+    def read_geos(self, geos_filename):
+        '''
+        read geos list from text file
+
+        Args:
+            geos_filename: str
+        '''
+        f_g = open(geos_filename, 'r')
+        geos = [line.replace('\n', '') for line in f_g]
+        f_g.close()
+        return geos
 
     def make(self):
         '''
