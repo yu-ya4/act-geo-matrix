52c0445a6df91d02831c8ce1a26d6c4c229f768c
yu <yuya4.matsumura@gmail.com>
2017-01-26 23:49:48 +0900

    Add TabelogReviews class, a list of TabelogReview

--- tabelog_review.py
+++ tabelog_review.py
@@ -29,3 +29,32 @@ class TabelogReview:
     @property
     def body(self):
         return self.__body
+
+class TabelogReviews:
+    '''
+    This class represents a list of Review.
+    '''
+
+    def __init__(self, reviews_path):
+        self.reviews = self.read_reviews(reviews_path)
+
+    def read_reviews(self, reviews_path):
+        '''
+        '''
+        f_urls = open(reviews_path + '/urls.txt', 'r')
+        urls = [line.replace('\n', '') for line in f_urls]
+        f_urls.close()
+
+        f_store_names = open(reviews_path + '/store_names.txt', 'r')
+        store_names = [line.replace('\n', '') for line in f_store_names]
+        f_store_names.close()
+
+        f_titles = open(reviews_path + '/titles.txt', 'r')
+        titles = [line.replace('\n', '') for line in f_titles]
+        f_titles.close()
+
+        f_bodies = open(reviews_path + '/bodies.txt', 'r')
+        bodies = [line.replace('\n', '') for line in f_bodies]
+        f_bodies.close()
+
+        return [TabelogReview(urls[i], store_names[i], titles[i], bodies[i]) for i in range(len(urls))]
