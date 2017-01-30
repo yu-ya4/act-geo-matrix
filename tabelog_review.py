665f71459bf7e52bc29c00ee6a1ad34ebfd5591b
yu <yuya4.matsumura@gmail.com>
2017-01-31 02:34:13 +0900

    Add error handler

--- tabelog_review.py
+++ tabelog_review.py
@@ -45,23 +45,24 @@ class TabelogReviews:
     def __read_reviews(self, reviews_path):
         '''
         '''
-        f_urls = open(reviews_path + 'urls.txt', 'r')
-        urls = [line.replace('\n', '') for line in f_urls]
-        f_urls.close()
-
-        f_store_names = open(reviews_path + 'store_names.txt', 'r')
-        store_names = [line.replace('\n', '') for line in f_store_names]
-        f_store_names.close()
-
-        f_titles = open(reviews_path + 'titles.txt', 'r')
-        titles = [line.replace('\n', '') for line in f_titles]
-        f_titles.close()
-
-        f_bodies = open(reviews_path + 'bodies.txt', 'r')
-        bodies = [line.replace('\n', '') for line in f_bodies]
-        f_bodies.close()
-
         try:
+
+            f_urls = open(reviews_path + 'urls.txt', 'r')
+            urls = [line.replace('\n', '') for line in f_urls]
+            f_urls.close()
+
+            f_store_names = open(reviews_path + 'store_names.txt', 'r')
+            store_names = [line.replace('\n', '') for line in f_store_names]
+            f_store_names.close()
+
+            f_titles = open(reviews_path + 'titles.txt', 'r')
+            titles = [line.replace('\n', '') for line in f_titles]
+            f_titles.close()
+
+            f_bodies = open(reviews_path + 'bodies.txt', 'r')
+            bodies = [line.replace('\n', '') for line in f_bodies]
+            f_bodies.close()
+
             reviews = [TabelogReview(urls[i], store_names[i], titles[i], bodies[i]) for i in range(len(urls))]
         except Exception as e:
             print(e)
