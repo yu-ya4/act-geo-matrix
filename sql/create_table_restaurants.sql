26b41d3805b16ecb180c871a968f5c46d78226fc
yu-ya4 <yuya4.matsumura@gmail.com>
2017-07-18 22:37:38 +0900

    Change column definitions

--- sql/create_table_restaurants.sql
+++ sql/create_table_restaurants.sql
@@ -2,19 +2,19 @@ USE matsumura_tabelog;
 
 CREATE TABLE restaurants(
     id int primary key AUTO_INCREMENT,
-    restaurant_id int UNIQUE,
-    name varchar(255),
+    restaurant_id int UNIQUE NOT NULL,
+    name varchar(255) NOT NULL,
     genre varchar(255),
     address varchar(255),
-    pal varchar(255),
-    LstPrf varchar(255),
-    LstAre varchar(255),
+    pal varchar(255) NOT NULL,
+    LstPrf varchar(255) NOT NULL,
+    LstAre varchar(255) NOT NULL,
     open_time varchar(255),
     regular_holiday varchar(255),
     budget varchar(255),
     budget_from_reviews varchar(255),
     seats varchar(255),
-    private_room varchar(255),
+    private_room text,
     private_use varchar(255),
     smoking varchar(255),
     space_or_facilities varchar(255),
@@ -28,4 +28,6 @@ CREATE TABLE restaurants(
     pr_comment_body text,
     url varchar(255),
     html longtext
+    created_at timestamp not null default current_timestamp,
+    updated_at timestamp not null default current_timestamp on update current_timestamp
 );
