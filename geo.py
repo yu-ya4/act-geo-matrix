bfaad51657e025a5326dfc45b7938ab1888eda1a
yu-ya4 <yuya4.matsumura@gmail.com>
2017-07-22 01:03:33 +0900

    Change property name from id to geo_id

--- geo.py
+++ geo.py
@@ -7,16 +7,16 @@ class Geo:
     The instance has an id(restaurant_id, unique in tabelog), a name, a url, a title and body of comment(pr in tabelog)
     '''
 
-    def __init__(self, id, name, url, title, body):
-        self.__id = id
+    def __init__(self, geo_id, name, url, title, body):
+        self.__geo_id = geo_id
         self.__name = name
         self.__url = url
         self.__title = title
         self.__body = body
 
     @property
-    def id(self):
-        return self.__id
+    def geo_id(self):
+        return self.__geo_id
 
     @property
     def name(self):
