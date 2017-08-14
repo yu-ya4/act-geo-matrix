ad9f59e8f807f200f80af516bc2854064c3bb172
yu-ya4 <yuya4.matsumura@gmail.com>
2017-08-14 23:54:23 +0900

    Change member variables of TabelogReview()

--- tabelog_review.py
+++ tabelog_review.py
@@ -6,23 +6,40 @@ import os
 class TabelogReview:
     '''
     This class reprents a review of tabelog.
-    The instance has a url, a store name, a title and a body of the review.
+    The instance has a review_id(unique), a restaurant_id, an url, a title and a body of the review.
 
     '''
 
-    def __init__(self, url, store_name, title, body):
+    def __init__(self, review_id, store_id, url, title, body):
+        '''
+        Args:
+            review_id: str
+            store_id: int
+            url: str
+            title: str
+            body: str
+
+        Ex:
+            tr = TabelogReview('B233597230', 26011529, 'http://tabelog.com/kyoto/A2601/A260605/26011529/dtlrvwlst/B233597230/?use_type=0&rvw_part=all&lc=2&smp=1',
+             'お刺身が美味しい！かす汁が熱々！軽く食べるには嬉しい居酒屋さんランチ', 'hogehoge')
+        '''
+        self.__review_id = review_id
+        self.__store_id = store_id
         self.__url = url
-        self.__store_name = store_name
         self.__title = title
         self.__body = body
 
     @property
-    def url(self):
-        return self.__url
+    def review_id(self):
+        return self.__review_id
 
     @property
-    def store_name(self):
-        return self.__store_name
+    def store_id(self):
+        return self.__store_id
+
+    @property
+    def url(self):
+        return self.__url
 
     @property
     def title(self):
