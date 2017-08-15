6e7fc0a5aeca7ec11a03143b096c7c6a9092688d
yu-ya4 <yuya4.matsumura@gmail.com>
2017-08-16 05:02:34 +0900

    Add read_geos_from_database()

--- geo.py
+++ geo.py
@@ -1,6 +1,10 @@
 #!usr/local/bin/python3
 # -*coding: utf-8
 
+import MySQLdb
+import traceback
+from configparser import ConfigParser
+
 class Geo:
     '''
     This class represents a geographic feature(a restaurant in tabelog).
@@ -8,6 +12,14 @@ class Geo:
     '''
 
     def __init__(self, geo_id, name, url, title, body):
+        '''
+        Args:
+            geo_id: int
+            name: str
+            url: str
+            title: str
+            body: str
+        '''
         self.__geo_id = geo_id
         self.__name = name
         self.__url = url
@@ -39,16 +51,48 @@ class Geos:
     a list of Geo
     '''
 
-    def __init__(self, geos):
+    def __init__(self):
         '''
         Args:
             geos: list[Geo]
         '''
-        self.__geos = geos
+        # self.__geos = geos
+        self.__geos = []
 
     @property
     def geos(self):
         return self.__geos
+
+    def read_geos_from_database(self):
+        '''
+        Read Geos from database(restaurants in tabelog)
+
+        Returns:
+            None
+        '''
+
+        self.__init__()
+        env = ConfigParser()
+        env.read('./.env')
+        try:
+            db_connection = MySQLdb.connect(host=env.get('mysql', 'HOST'), user=env.get('mysql', 'USER'), passwd=env.get('mysql', 'PASSWD'), db=env.get('mysql', 'DATABASE'), charset=env.get('mysql', 'CHARSET'))
+            cursor = db_connection.cursor()
+            sql = 'SELECT id, restaurant_id, name, url, pr_comment_title, pr_comment_body FROM restaurants;'
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            for row in result:
+                geo = Geo(int(row[1]), row[2], row[3], row[4], row[5])
+                self.__geos.append(geo)
+
+        except MySQLdb.Error as e:
+            print('MySQLdb.Error: ', e)
+
+        except Exception as e:
+            traceback.print_exc()
+            print(e)
+
+        cursor.close()
+        db_connection.close()
 
     def append(self, another_geo):
         '''
