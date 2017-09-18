5009c801faae38259e218a08c97ef2c49caebf68
yu-ya4 <yuya4.matsumura@gmail.com>
2017-09-19 04:24:23 +0900

    Addlsa()

--- experience_geo_matrix.py
+++ experience_geo_matrix.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8
 
 import numpy as np
+from scipy import linalg
 
 class ExperienceGeoMatrix:
     '''
@@ -50,6 +51,29 @@ class ExperienceGeoMatrix:
             return print('no index \n')
 
         return self.scores[experience_index]
+
+    def convert_experience_vector_to_result_list(self, experience_vec, result_num=10):
+        '''
+        Args:
+            vec: numpy.ndarray[numpy.float64]
+        Returns:
+
+        '''
+        result_list = []
+        ranking = sorted([(v,i) for (i,v) in enumerate(experience_vec)])
+
+        for i in range(result_num+1):
+            if i == 0:
+                continue
+            geo_index = ranking[-i][1]
+            geo = self.geos.geos[geo_index]
+            score = float(ranking[-i][0])
+
+            if score == 0:
+                break
+            result_list.append((geo.geo_id, score))
+
+        return result_list
 
     def show_geo_ranking_by_vector(self, experience_vec, result_num):
         '''
@@ -239,3 +263,50 @@ class ExperienceGeoMatrix:
             if np.amax(row) * 1 != 0:
                 self.scores[i] = row / np.amax(row) * 1
             i += 1
+
+    def svd(self):
+        """
+        行列を特異値分解(SVD)する
+
+        Returns:
+            List<numpy.matrix>
+            特異値分解された行列3つ
+            特異値はリストで返される
+        """
+        #mat = np.matrix(mat)
+
+        """
+        full_matrices:
+            1: UとVが正方行列に(次元が合わずに死ぬ??)
+            0: UとVのかたちをいい感じに(とりあえずこれでなんとかしてる)
+        """
+        U, s, V = np.linalg.svd(self.scores, full_matrices=0)
+        #print(s)
+
+        return [U, s, V]
+
+    def lsa(self, k):
+        """
+        行列にLSAを適用する
+
+        Returns:
+            numpy.matrix
+            LSAを適用した結果
+        """
+        rank = np.linalg.matrix_rank(self.scores)
+        # ランク以上の次元数を指定した場合は，ランク数分の特徴量を使用
+        #npの仕様上，ランク以上分の特徴量を算出してるっぽい？
+        # 負の値が入力された場合はk = 0 とする
+        U, s, V = self.svd()
+        if k > rank:
+            k = rank
+        if k < 0:
+            k = 0
+
+        #print("ランクk=%d 累積寄与率=%f" % (k, sum(s[:k]) / sum(s)))
+        S = np.zeros((len(s),len(s)))
+        S[:k, :k] = np.diag(s[:k]) #上からk個の特異値のみを使用
+
+        lsa_mat = np.dot(U, np.dot(S, V))
+
+        self.scores = lsa_mat
