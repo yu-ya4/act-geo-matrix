58edb3c3504cbb39a41e3986a56a082d6276fef1
yu-ya4 <yuya4.matsumura@gmail.com>
2017-11-12 18:38:27 +0900

    Add conde to collect reviews

--- main.py
+++ main.py
@@ -415,52 +415,50 @@ if __name__ == '__main__':
     # print(restaurants)
 
 
-    tls = TabelogSearcher('ieyasu')
-    saved_area_list = tls.get_saved_area_in_db()
-    area_list = tls.get_area_list_from_db()
-    # print(saved_area_list)
-    # print(area_list)
-    # exit()
-    # restaurant_urls = tls.get_restaurant_urls_from_db(10,3669)
-    # cat_list = [('BC', 'BC01', ''), ('BC', 'BC02', ''), ('BC', 'BC03', ''), ('BC', 'BC04', ''), ('BC', 'BC05', ''), ('BC', 'BC06', ''), ('BC', 'BC07', ''), ('BC', 'BC99', ''),
-    #     ('RC', 'RC21', 'RC2101'), ('RC', 'RC21', 'RC2102'), ('RC', 'RC21', 'RC2199')]
-    cat_list = [('MC', 'MC01', ''), ('MC', 'MC11', ''), ('MC', 'MC21', ''),
-                ('CC', 'CC01', ''), ('CC', 'CC02', ''), ('CC', 'CC03', ''), ('CC', 'CC04', ''), ('CC', 'CC05', ''), ('CC', 'CC06', ''), ('CC', 'CC99', ''),
-                ('SC', 'SC01', 'SC0101'), ('SC', 'SC01', 'SC0102'), ('SC', 'SC01', 'SC0103'), ('SC', 'SC01', 'SC0199'),
-                ('SC', 'SC02', 'SC0201'), ('SC', 'SC02', 'SC0202'), ('SC', 'SC02', 'SC0203'), ('SC', 'SC02', 'SC0299'),
-                ('YC', 'YC01', ''), ('YC', 'YC02', ''), ('YC', 'YC99', ''),
-                ('RC', 'RC01', 'RC0101'), ('RC', 'RC01', 'RC0102'), ('RC', 'RC01', 'RC0103'), ('RC', 'RC01', 'RC0104'), ('RC', 'RC01', 'RC0105'), ('RC', 'RC01', 'RC0106'), ('RC', 'RC01', 'RC0107'), ('RC', 'RC01', 'RC0108'), ('RC', 'RC01', 'RC0109'), ('RC', 'RC01', 'RC0110'), ('RC', 'RC01', 'RC0111'), ('RC', 'RC01', 'RC0112'), ('RC', 'RC01', 'RC0199'),
-                ('RC', 'RC02', 'RC0201'), ('RC', 'RC02', 'RC0202'), ('RC', 'RC02', 'RC0203'), ('RC', 'RC02', 'RC0204'), ('RC', 'RC02', 'RC0209'), ('RC', 'RC02', 'RC0211'), ('RC', 'RC02', 'RC0212'), ('RC', 'RC02', 'RC0213'), ('RC', 'RC02', 'RC0219'),
-                ('RC', 'RC03', 'RC0301'), ('RC', 'RC03', 'RC0302'), ('RC', 'RC03', 'RC0303'), ('RC', 'RC03', 'RC0304'),
-                ('RC', 'RC04', 'RC0401'), ('RC', 'RC04', 'RC0402'), ('RC', 'RC04', 'RC0403'), ('RC', 'RC04', 'RC0404'), ('RC', 'RC04', 'RC0411'), ('RC', 'RC04', 'RC0412'), ('RC', 'RC04', 'RC0499'),
-                ('RC', 'RC12', 'RC1201'), ('RC', 'RC12', 'RC1202'), ('RC', 'RC12', 'RC1203'), ('RC', 'RC12', 'RC1204'), ('RC', 'RC12', 'RC1205'), ('RC', 'RC12', 'RC1299'),
-                ('RC', 'RC13', 'RC1301'), ('RC', 'RC13', 'RC1302'),
-                ('RC', 'RC14', 'RC1401'), ('RC', 'RC14', 'RC1402'), ('RC', 'RC14', 'RC1403'), ('RC', 'RC14', 'RC1404'), ('RC', 'RC14', 'RC1405'), ('RC', 'RC14', 'RC1406'), ('RC', 'RC14', 'RC1407'), ('RC', 'RC14', 'RC1408'), ('RC', 'RC14', 'RC1409'),
-                ('RC', 'RC22', 'RC2201'), ('RC', 'RC22', 'RC2202'), ('RC', 'RC22', 'RC2203'),
-                ('RC', 'RC23', ''), ('RC', 'RC99', 'RC9901'), ('RC', 'RC99', 'RC9904'), ('RC', 'RC99', 'RC9903'), ('RC', 'RC99', 'RC9999')
-                ]
-
-    for area in area_list:
-        if (area[0] == 'kyoto' and area[1] == 'A2601'):
-            print(area)
-            for cat in cat_list:
-                print(cat)
-                restaurant_htmls = tls.search_for_restaurants('', area[0], area[1], area[2], cat[0], cat[1], cat[2], '')
-                restaurants = tls.parse_restaurants(restaurant_htmls[0], restaurant_htmls[1])
-                tls.save_restaurants(restaurants)
-    exit()
-
-
-
     # tls = TabelogSearcher('ieyasu')
     # saved_area_list = tls.get_saved_area_in_db()
-    # # restaurant_urls = [
-    # #     'http://tabelog.com/kyoto/A2601/A260603/26011408/'
-    # #     ]
-    # restaurant_urls = tls.get_restaurant_urls_from_db(100000, 0, 'A270501')
+    # area_list = tls.get_area_list_from_db()
+    # # print(saved_area_list)
+    # # print(area_list)
+    # # exit()
+    # # restaurant_urls = tls.get_restaurant_urls_from_db(10,3669)
+    # # cat_list = [('BC', 'BC01', ''), ('BC', 'BC02', ''), ('BC', 'BC03', ''), ('BC', 'BC04', ''), ('BC', 'BC05', ''), ('BC', 'BC06', ''), ('BC', 'BC07', ''), ('BC', 'BC99', ''),
+    # #     ('RC', 'RC21', 'RC2101'), ('RC', 'RC21', 'RC2102'), ('RC', 'RC21', 'RC2199')]
+    # cat_list = [('MC', 'MC01', ''), ('MC', 'MC11', ''), ('MC', 'MC21', ''),
+    #             ('CC', 'CC01', ''), ('CC', 'CC02', ''), ('CC', 'CC03', ''), ('CC', 'CC04', ''), ('CC', 'CC05', ''), ('CC', 'CC06', ''), ('CC', 'CC99', ''),
+    #             ('SC', 'SC01', 'SC0101'), ('SC', 'SC01', 'SC0102'), ('SC', 'SC01', 'SC0103'), ('SC', 'SC01', 'SC0199'),
+    #             ('SC', 'SC02', 'SC0201'), ('SC', 'SC02', 'SC0202'), ('SC', 'SC02', 'SC0203'), ('SC', 'SC02', 'SC0299'),
+    #             ('YC', 'YC01', ''), ('YC', 'YC02', ''), ('YC', 'YC99', ''),
+    #             ('RC', 'RC01', 'RC0101'), ('RC', 'RC01', 'RC0102'), ('RC', 'RC01', 'RC0103'), ('RC', 'RC01', 'RC0104'), ('RC', 'RC01', 'RC0105'), ('RC', 'RC01', 'RC0106'), ('RC', 'RC01', 'RC0107'), ('RC', 'RC01', 'RC0108'), ('RC', 'RC01', 'RC0109'), ('RC', 'RC01', 'RC0110'), ('RC', 'RC01', 'RC0111'), ('RC', 'RC01', 'RC0112'), ('RC', 'RC01', 'RC0199'),
+    #             ('RC', 'RC02', 'RC0201'), ('RC', 'RC02', 'RC0202'), ('RC', 'RC02', 'RC0203'), ('RC', 'RC02', 'RC0204'), ('RC', 'RC02', 'RC0209'), ('RC', 'RC02', 'RC0211'), ('RC', 'RC02', 'RC0212'), ('RC', 'RC02', 'RC0213'), ('RC', 'RC02', 'RC0219'),
+    #             ('RC', 'RC03', 'RC0301'), ('RC', 'RC03', 'RC0302'), ('RC', 'RC03', 'RC0303'), ('RC', 'RC03', 'RC0304'),
+    #             ('RC', 'RC04', 'RC0401'), ('RC', 'RC04', 'RC0402'), ('RC', 'RC04', 'RC0403'), ('RC', 'RC04', 'RC0404'), ('RC', 'RC04', 'RC0411'), ('RC', 'RC04', 'RC0412'), ('RC', 'RC04', 'RC0499'),
+    #             ('RC', 'RC12', 'RC1201'), ('RC', 'RC12', 'RC1202'), ('RC', 'RC12', 'RC1203'), ('RC', 'RC12', 'RC1204'), ('RC', 'RC12', 'RC1205'), ('RC', 'RC12', 'RC1299'),
+    #             ('RC', 'RC13', 'RC1301'), ('RC', 'RC13', 'RC1302'),
+    #             ('RC', 'RC14', 'RC1401'), ('RC', 'RC14', 'RC1402'), ('RC', 'RC14', 'RC1403'), ('RC', 'RC14', 'RC1404'), ('RC', 'RC14', 'RC1405'), ('RC', 'RC14', 'RC1406'), ('RC', 'RC14', 'RC1407'), ('RC', 'RC14', 'RC1408'), ('RC', 'RC14', 'RC1409'),
+    #             ('RC', 'RC22', 'RC2201'), ('RC', 'RC22', 'RC2202'), ('RC', 'RC22', 'RC2203'),
+    #             ('RC', 'RC23', ''), ('RC', 'RC99', 'RC9901'), ('RC', 'RC99', 'RC9904'), ('RC', 'RC99', 'RC9903'), ('RC', 'RC99', 'RC9999')
+    #             ]
     #
-    # print(len(restaurant_urls))
-    # restaurant_urls = restaurant_urls[421:]
+    # for area in area_list:
+    #     if (area[0] == 'kyoto' and area[1] == 'A2601'):
+    #         print(area)
+    #         for cat in cat_list:
+    #             print(cat)
+    #             restaurant_htmls = tls.search_for_restaurants('', area[0], area[1], area[2], cat[0], cat[1], cat[2], '')
+    #             restaurants = tls.parse_restaurants(restaurant_htmls[0], restaurant_htmls[1])
+    #             tls.save_restaurants(restaurants)
+    # exit()
+
+
+
+    tls = TabelogSearcher('ieyasu')
+    saved_area_list = tls.get_saved_area_in_db()
+    # restaurant_urls = [
+    #     'http://tabelog.com/kyoto/A2601/A260603/26011408/'
+    #     ]
+    # restaurant_urls = tls.get_restaurant_urls_without_reviews_from_db(1000, 0, 'A270501')
+    #
     # print(len(restaurant_urls))
     # flg = True
     # for restaurant_url in restaurant_urls:
@@ -479,22 +477,30 @@ if __name__ == '__main__':
 
 
 
-    # tls = TabelogSearcher('ieyasu')
-    # saved_area_list = tls.get_saved_area_in_db()
-    # # restaurant_urls = [
-    # #     'http://tabelog.com/kyoto/A2601/A260603/26011408/'
-    # #     ]
-    # for saved_area in saved_area_list:
-    #     restaurant_urls = tls.get_restaurant_urls_from_db(100000, 0, saved_area)
-    #     print(saved_area)
-    #
-    #     for restaurant_url in restaurant_urls:
-    #         review_htmls = tls.get_reviews_from_restaurant(restaurant_url)
-    #         reviews = tls.parse_reviews(review_htmls[0], review_htmls[1])
-    #         print(len(reviews))
-    #         tls.save_reviews(reviews)
-    # tls.db_connection.close()
-    # exit()
+    tls = TabelogSearcher('ieyasu')
+    saved_area_list = tls.get_saved_area_in_db()
+    # restaurant_urls = [
+    #     'http://tabelog.com/kyoto/A2601/A260603/26011408/'
+    #     ]
+    flg = False
+    skip_list = ['A260605', 'A260604', 'A260603', 'A260602']
+    for saved_area in saved_area_list:
+        if saved_area in skip_list:
+            continue
+        restaurant_urls = tls.get_restaurant_urls_without_reviews_from_db(100000, 0, saved_area)
+        print(saved_area)
+        print(len(restaurant_urls))
+        if saved_area == 'A260201':
+            restaurant_urls += 'https://tabelog.com/kyoto/A2601/A260201/26005726/'
+
+        for restaurant_url in restaurant_urls:
+
+            review_htmls = tls.get_reviews_from_restaurant(restaurant_url)
+            reviews = tls.parse_reviews(review_htmls[0], review_htmls[1])
+            print(len(reviews))
+            tls.save_reviews(reviews)
+    tls.db_connection.close()
+    exit()
     # reviews.write_review('./reviews/20170607/飲む/')
     # exit()
     # tr = TabelogReview(1, '鳥貴族 出町柳前店', '爆飲みするなら！', '出町柳周辺で学生さんが爆飲みするのにピッタシなお店です．')
